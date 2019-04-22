48df0a941c819db3400757dac51d5bc9c49eae5f
Jordan Williams <Oryinn@jordans-mbp-6.lan>
2019-04-22 19:04:09 -0400

    update wsgi to client

--- client/wsgi.py
+++ client/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beer_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'client.settings')
 
 application = get_wsgi_application()
